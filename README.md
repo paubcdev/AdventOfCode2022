df0ffcc716428a999a7a82b1411cf73e0cbb4b6b
paubcdev <blanescolomina.pau@gmail.com>
2022-12-07 12:57:53 +0100

    AOC2022, day7

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ This will be the README and log file for my attemp on the Advent of Code 2022. I
 * **Day 4:** Let's clean the site! :star2: :star2:
 * <ins>**Day 5:** CRATEMOVER 9001!</ins> :star2: :star2:
 * **Day 6:** Communication Breakdown :star2: :star2:
+* **Day 7:** File system shenanigans :star2: :star2:
 
 ***
-## Number of starts collected: 12!
+
+## Number of starts collected
+
+14!
---
+++ day07/src/main.rs
@@ -0,0 +1,143 @@
+use std::{collections::HashMap, error::Error};
+use parse_display::{Display, FromStr};
+
+struct FileSystem{
+    tree: HashMap<String, Directory>
+}
+
+impl FileSystem {
+    fn dir_size(&self, dir: &String) -> Option<i64> {
+        if let Some(dir) = self.tree.get(dir) {
+            let file_total = dir.files.iter()
+                    .map(|f| f.size)
+                    .sum::<i64>();
+            let dir_total = dir.children.iter().map(|d| self.dir_size(d).unwrap_or(0)).sum::<i64>();
+    
+            Some(file_total + dir_total)
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Debug,Clone)]
+struct Directory {
+    files: Vec<FileMetadata>,
+    children: Vec<String>
+}
+
+
+#[derive(Display, FromStr, PartialEq, Debug, Clone)]
+#[display("{size} {name}")]
+struct FileMetadata {
+    name: String,
+    size: i64
+}
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let data = include_str!("../../inputs/day07.txt");
+    
+    println!("Part 1: {}", solve_part1(data));
+    println!("Part 2: {}", solve_part2(data));
+    Ok(())
+}
+
+fn solve_part1(data: &str) -> i64 {
+    let fs = parse_tree(data);
+    let mut result = 0;
+    for dir in fs.tree.keys() {
+        if let Some(size) = fs.dir_size(dir){
+            if size <= 100000 {
+                result += size
+            }
+        }
+    }
+    result
+}
+
+fn solve_part2(data: &str) -> i64 {
+    let fs = parse_tree(data);
+    let free_space = 70000000 - fs.dir_size(&String::from("")).unwrap();
+    let mut result_2 = i64::MAX;
+    for dir in fs.tree.keys() {
+        if let Some(size) = fs.dir_size(dir) {
+            if free_space + size >= 30000000 {
+                result_2 = result_2.min(size)
+            }
+        }
+    }
+    result_2
+}
+
+
+fn parse_tree(data:&str)  -> FileSystem {
+    let mut dir_stack = vec![];
+    let mut tree = HashMap::new();
+    for command in data[2..].split("\n$ ") {
+        if command == "cd /" {
+            dir_stack.clear();
+            dir_stack.push("");
+        } else if command == "cd .." {
+            dir_stack.pop();
+        } else if let Some(path) = command.strip_prefix("cd "){
+            dir_stack.push(path)
+        } else if command.starts_with("ls") {
+            let mut results = command.lines();
+            results.next();
+            let mut children = vec![];
+            let mut files = vec![];
+            for line in results {
+                if let Some(dirname) = line.strip_prefix("dir ") {
+                    children.push(format!("{}/{}", dir_stack.join("/"),dirname));
+                } else {
+                    files.push(line.parse::<FileMetadata>().unwrap());
+                }
+            }
+            tree.insert(dir_stack.join("/"),Directory { files, children });
+            
+        }
+    }
+    FileSystem{tree}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const DATA: &str = "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+";
+
+    #[test]
+    fn test_part_1() {
+        let part1 = solve_part1(DATA);
+        assert_eq!(part1, 95437);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let part2 = solve_part2(DATA);
+        assert_eq!(part2, 24933642);
+    }
+}
